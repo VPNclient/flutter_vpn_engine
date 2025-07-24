81129708cc45d5b9584b0472a0cb645c5d8d7290
Phakin Kongkha <secured.ntrain@gmail.com>
2025-07-24 23:34:46 +0700

    Implement example UI

--- android/vpnclient-engine/src/main/java/click/vpnclient/engine/VpnClientEngine.java
+++ android/vpnclient-engine/src/main/java/click/vpnclient/engine/VpnClientEngine.java
@@ -1,4 +1,54 @@
 package click.vpnclient.engine;
 
 public class VpnClientEngine {
+    private static final String TAG = "VpnClientEngine";
+    private static boolean isRunning = false;
+    private final DriverType driver;
+    public VpnClientEngine(DriverType driverType) {
+        this.driver = driverType;
+        switch (this.driver) {
+            case LibXray:
+                Log.i(TAG, "Using LibXray driver" + " version: " + LibXray.xrayVersion());
+                break;
+            default:
+                throw new IllegalArgumentException("Unsupported driver type: " + driverType);
+        }
+    }
+
+    public static String getVersion() {
+        return LibXray.xrayVersion();
+    }
+
+    public boolean start(String dataDir, String config) {
+        if (isRunning) {
+            Log.w(TAG, "Engine is already running");
+            return false;
+        }
+        try {
+            switch (this.driver) {
+                case LibXray:
+                    LibXray.runXrayFromJSON(LibXray.newXrayRunFromJSONRequest(dataDir, config));
+                    break;
+                default:
+                    throw new IllegalStateException("Unsupported driver type: " + this.driver);
+            }
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    public void stop() {
+        if (!isRunning) return;
+        LibXray.stopXray();
+        isRunning = false;
+    }
+
+    public boolean isRunning() {
+        return LibXray.getXrayState();
+    }
+
+    public enum DriverType {
+        LibXray
+    }
 }
